2d5cb01623e3d0061253e2f3fea9111aa6d451e5
KKimguhyun <guhyun9236@nate.com>
2019-03-03 17:26:10 +0900

    프로젝트 커밋 // WeakenedRPG.uproject 우클릭 -> Generated visual studio project file 누르고 sln 파일 생기면 실행하면 됨 // 소스코드 새로 추가될때마다 Generated visual studio project file 해줘야 추가한 소스코드가 솔루션에 추가 됨

---
+++ WeakenedRPG/Source/WeakenedRPG/WeakenedRPGGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WeakenedRPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WEAKENEDRPG_API AWeakenedRPGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
